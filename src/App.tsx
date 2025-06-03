ea30f8255f1dc64b45f0148b7899c16dbfb55e8e
pelzman <oluwapelumiadeyemo72@gmail.com>
2025-06-03 18:34:55 +0100

    boiler-plate

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <Button className="bg-blue-500 text-white hover:bg-blue-600">
+          Click Me
+        </Button>
+      </div>
+
+    </>
+  )
+}
+
+export default App  
